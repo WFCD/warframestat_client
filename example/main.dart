2900f067b8ca7afb2ca5240a657a1fb9c273d4af
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-01-22 01:30:58 -0500

    chore: fix example

--- example/main.dart
+++ example/main.dart
@@ -2,7 +2,7 @@ import 'package:warframestat_client/warframestat_client.dart';
 
 void main() async {
   final ws = WorldstateClient();
-  final cs = await ws.currentState();
+  final cs = await ws.fetchWorldstate();
 
   // ignore: avoid_print
   print(cs.timestamp);
