a74f4dd1a571589668f1a262bb4ed8a716d46aec
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-02-05 17:47:20 -0500

    test: added test for `MinimalItem`

--- test/src/models/items_test.dart
+++ test/src/models/items_test.dart
@@ -7,6 +7,7 @@ import '../../helpers/fixtures.dart';
 
 void main() {
   late List<Map<String, dynamic>> jsonMap;
+  late List<Map<String, dynamic>> minimalItem;
 
   setUp(() async {
     final fixture = await ItemsFixture().loadWarframeItems();
@@ -14,11 +15,29 @@ void main() {
     jsonMap = List<dynamic>.from(json.decode(fixture) as List<dynamic>)
         .map((e) => e as Map<String, dynamic>)
         .toList();
+
+    // uniqueName,name,description,imageName,category,type,vaulted,vaultDate
+    minimalItem = jsonMap.map((e) {
+      final vaulted = e['vaulted'];
+      final vaultDate = e['vaultDate'];
+
+      return {
+        'uniquName': e['uniqueName'],
+        'name': e['name'],
+        'description': e['description'],
+        'imageName': e['imageName'],
+        'category': e['category'],
+        'type': e['type'],
+        if (vaulted != null) 'vaulted': vaulted,
+        if (vaultDate != null) 'vaultDate': vaultDate,
+      };
+    }).toList();
   });
 
-  test('Serlization checks', () {
-    final items = toItems(jsonMap);
+  test('Serlization checks for [Item]', () => expect(toItems(jsonMap), isList));
 
-    expect(items, isList);
-  });
+  test(
+    'Serlization checks for [MinimalItem]',
+    () => expect(jsonMap.map(MinimalItem.fromJson).toList(), isList),
+  );
 }
