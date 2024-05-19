41198f1034c165cc75f596cbe750e6eb0fdca9ab
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2024-05-19 01:26:52 -0400

    test: add checks for profile serlization

--- test/scripts/update_fixtures.sh
+++ test/scripts/update_fixtures.sh
@@ -8,3 +8,6 @@ wget https://github.com/WFCD/warframe-items/raw/master/data/json/All.json -O ./t
 
 # Get warframe-items enemy json.
 wget https://raw.githubusercontent.com/WFCD/warframe-items/master/data/json/Enemy.json -O ./test/fixtures/enemy.json
+
+# Get a player profile
+wget https://api.warframestat.us/profile/OrnsteinTheSlayer/ -O ./test/fixtures/OrnsteinTheSlayer.json
