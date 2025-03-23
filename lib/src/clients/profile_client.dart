7cf93c639c423022f1bef417fe1540c6a8d1d18b
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-03-23 12:56:46 -0400

    fix: unmark profile endpoints as deprecated but leave an unstable comment in docs

--- lib/src/clients/profile_client.dart
+++ lib/src/clients/profile_client.dart
@@ -5,25 +5,25 @@ import 'package:warframestat_client/warframestat_client.dart';
 
 /// {@template profile_client}
 /// Profile client to interact with the profile endpoint.
+///
+/// Unstable as DE can change or remove things at anytime
 /// {@endtemplate}
 class ProfileClient extends WarframestatClient {
   /// {@macro profile_client}
-  ProfileClient({required this.username, super.language, super.ua, super.client});
+  ProfileClient({required this.accountId, super.language, super.ua, super.client});
 
   /// Player username
-  final String username;
+  final String accountId;
 
   /// Will fetch the player's profile that will include their current loadout
   /// (both arsenal and operator) and items that contributed to player's
   /// mastery rank
-  @Deprecated('DE put the profile endpoint behind auth so all calls will time out')
   Future<Profile> fetchProfile() async {
     return _fetch<Map<String, dynamic>, Profile>('/', Profile.fromJson);
   }
 
   /// Will fetch just the item that have contributed to the player's
   /// mastery rank
-  @Deprecated('DE put the profile endpoint behind auth so all calls will time out')
   Future<List<XpItem>> fetchXpInfo() async {
     return _fetch<List<dynamic>, List<XpItem>>(
       '/xpInfo',
@@ -32,7 +32,7 @@ class ProfileClient extends WarframestatClient {
   }
 
   Future<S> _fetch<T, S>(String path, S Function(T) parser) async {
-    final response = await get('profile/$username$path');
+    final response = await get('profile/$accountId$path');
 
     return Isolate.run(() => parser(json.decode(response.body) as T));
   }
